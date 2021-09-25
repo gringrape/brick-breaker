af1a2b870cece15b50027eb68bf1ef2927667124
gringrape <gringrape200@gmail.com>
2021-09-25 23:22:42 +0900

    Create paddle
    Create paddle and move it according to keyboard input
    
    source: https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Paddle_and_keyboard_controls

--- src/moves.js
+++ src/moves.js
@@ -1,5 +1,7 @@
 function reducer(state, action) {
-  const { x, y, velocity } = state;
+  const {
+    x, y, velocity, paddleX, leftPressed, rightPressed,
+  } = state;
   const { dx, dy } = velocity;
 
   return ({
@@ -28,9 +30,54 @@ function reducer(state, action) {
         },
       });
     },
+    pressRight() {
+      return ({
+        ...state,
+        rightPressed: true,
+      });
+    },
+    pressLeft() {
+      return ({
+        ...state,
+        leftPressed: true,
+      });
+    },
+    releaseRight() {
+      return ({
+        ...state,
+        rightPressed: false,
+      });
+    },
+    releaseLeft() {
+      return ({
+        ...state,
+        leftPressed: false,
+      });
+    },
+    paddleMove() {
+      let direction = 0;
+
+      if (leftPressed) {
+        direction = -1;
+      }
+
+      if (rightPressed) {
+        direction = 1;
+      }
+
+      return ({
+        ...state,
+        paddleX: paddleX + direction * 3,
+      });
+    },
   })[action]();
 }
 
 export const move = (state) => reducer(state, 'move');
 export const hitHorizontal = (state) => reducer(state, 'hitHorizontal');
 export const hitVertical = (state) => reducer(state, 'hitVertical');
+export const pressRight = (state) => reducer(state, 'pressRight');
+export const pressLeft = (state) => reducer(state, 'pressLeft');
+export const releaseRight = (state) => reducer(state, 'releaseRight');
+export const releaseLeft = (state) => reducer(state, 'releaseLeft');
+export const paddleMove = (state) => reducer(state, 'paddleMove');
